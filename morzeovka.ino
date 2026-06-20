32cf44f259a3995acd0b2bdca441776961f608a7
michalbrinza <michal.brinza@gmail.com>
2026-06-20 18:05:05 +0200

    Update morzeovka.ino

--- morzeovka.ino
+++ morzeovka.ino
@@ -1,8 +1,8 @@
 
-const int buttonPin = 2; // Connect button to pin 2 and GND
+const int buttonPin = 2; 
 
-const int dashThreshold = 250;  // Longer than this is a dash, shorter is a dot
-const int spaceThreshold = 1000; // Wait this long for a space between letters
+const int dashThreshold = 250;  
+const int spaceThreshold = 1000; 
 
 unsigned long pressStartTime = 0;
 unsigned long releaseTime = 0;
@@ -17,40 +17,37 @@ void setup() {
 }
 
 void loop() {
-  // Read the button (LOW means pressed)
   bool currentButtonState = digitalRead(buttonPin);
 
-  // 1. Button was just PRESED
+
   if (currentButtonState == LOW && lastButtonState == HIGH) {
     pressStartTime = millis();
     isPressed = true;
-    spacePrinted = false; // We are typing, so we don't need a space yet
-    delay(50); // Debounce delay to stop button bounce
+    spacePrinted = false; 
+    delay(50); 
   }
 
-  // 2. Button was just RELEASED
+
   if (currentButtonState == HIGH && lastButtonState == LOW) {
     unsigned long pressDuration = millis() - pressStartTime;
-    releaseTime = millis(); // Start timing the pause
+    releaseTime = millis();
     isPressed = false;
 
-    // Check if it was a dot or a dash
+   
     if (pressDuration > dashThreshold) {
-      Serial.print("-"); // Dash
-    } else if (pressDuration > 10) { // Ignore tiny accidental touches
-      Serial.print("."); // Dot
+      Serial.print("-"); 
+    } else if (pressDuration > 10) { 
+      Serial.print("."); 
     }
-    delay(50); // Debounce delay
+    delay(50); 
   }
 
-  // 3. Button is NOT pressed (Check for spaces between letters)
   if (currentButtonState == HIGH && !isPressed && !spacePrinted) {
     if (millis() - releaseTime > spaceThreshold) {
-      Serial.print(" "); // Print a space to separate letters
+      Serial.print(" "); 
       spacePrinted = true;
     }
   }
 
-  // Save the state for the next loop
   lastButtonState = currentButtonState;
 }
